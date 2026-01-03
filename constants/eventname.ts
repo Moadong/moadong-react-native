8f6103c67dafe19c1b0aa6b5e07948973b5a1f71
seongwon seo <seongwon0903@gmail.com>
2026-01-03 12:15:05 +0900

    feat: Mixpanel 이벤트명 상수 정의
    - 사용자 액션 이벤트 (클릭, 검색 등)
    - 페이지 뷰 이벤트 정의
    - 이벤트명 일관성 유지를 위한 상수화

---
+++ constants/eventname.ts
@@ -0,0 +1,12 @@
+export const USER_EVENT = {
+  CATEGORY_BUTTON_CLICKED: 'CategoryButton Clicked',
+  SEARCH_BOX_CLICKED: 'SearchBox Clicked',
+  BANNER_CLICKED: 'Banner Clicked',
+  CLUB_CARD_CLICKED: 'ClubCard Clicked',
+  SUBSCRIBE_BUTTON_CLICKED: 'SubscribeButton Clicked',
+  TAB_CHANGED: 'Tab Changed',
+}
+
+export const PAGE_VIEW_EVENT = {
+  MAIN_PAGE: 'MainPage',
+}
